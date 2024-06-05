7385a3e9fc0bc7d606657608244fc2c022ef686b
habibur-rahmam <habiburrahmanmunna56@gmail.com>
2024-06-05 16:05:05 +0600

    React Project

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,36 @@ import Footer from "./Layout/Footer";
 
 function App() {
   const [productslist, setProductslist] = useState();
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
     const fetchProducts = async () => {
-      const response = await fetch("https://dummyjson.com/products");
-      const data = await response.json();
-      setProductslist(data);
+     
     };
 
     fetchProducts();
-  }, []);
+  }, [count]);
+
+  const fetchproductbyClick = () => {
+    setCount(count + 1);
+    setTimeout(async() => {
+      const response = await fetch("https://dummyjson.com/products");
+      const data = await response.json();
+      setProductslist(data);
+    }, 1000); 
+  };
+
   return (
     <div className="max-w-[1140px] m-auto  ">
       <Header />
       <h1 className="text-lg font-bold text-center my-10 uppercase">All Product List</h1>
- 
+      
       {productslist ? (
 <ProductList productslist={productslist} />
       ) : (
-        <p className="font-bold text-xxl capitalize">Loading  All Productt..</p>
+        <p className="font-bold text-base uppercase">fetch product by Click</p>
       )}
+      <button className="bg-white text-green-500 border border-green-500 font-medium py-2 px-4 rounded capitalize mt-5 mb-5" onClick={fetchproductbyClick}>see all product</button>
       <Footer />
     </div>
   );
