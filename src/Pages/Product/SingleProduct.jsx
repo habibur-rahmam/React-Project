367c535b04531313b522f5550ae2411fc5d3fcd9
habibur-rahmam <habiburrahmanmunna56@gmail.com>
2024-06-09 17:48:33 +0600

    React Project

--- src/Pages/Product/SingleProduct.jsx
+++ src/Pages/Product/SingleProduct.jsx
@@ -28,7 +28,6 @@ const SingleProduct = () => {
                 <p className="w-2/4 text-center text-xxl mt-5 mb-5">{product.description}</p>
                 <div className="flex justify-between gap-10 items-center border-2 p-4 mb-2">
                 <p className="text-center text-lg ">Rating: {product.rating}</p>
-                <p className=" text-center text-lg">Tag: {product.tags}</p>
                 <p className=" text-center text-lg">Brand : {product.brand}</p>
                 <p className="text-lg">Price: ${product.price}</p>
                 </div>
