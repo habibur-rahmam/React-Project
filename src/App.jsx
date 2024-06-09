c79c64c12e8f6405eb87149e0e67feb2a8f2dce7
habibur-rahmam <habiburrahmanmunna56@gmail.com>
2024-06-09 13:17:41 +0600

    React Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import Footer from "./Layout/Footer";
-import Header from "./Layout/Header";
 import Router from "./Router/Router";
-
 const App = () => {
   return (
       <div>
-        <Header/>
         <Router />
-        <Footer/>
       </div>
   );
 };
-
 export default App;
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
-import Product from '../Pages/Home'; 
 import Home from '../Pages/Home';
+import Layout from '../Layout/Layout';
+
 const Router = () => {
   return (
-    <Routes>
-      <Route path="/AllProduct" element={<Product />} />
-      <Route path="/Home" element={<Home />} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path="/Home" element={<Home />} />
+      </Routes>
+    </Layout>
   );
 };
 
